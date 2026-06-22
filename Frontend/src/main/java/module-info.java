46f1377c5392490f60ffe71f382a384ac71b3fc9
Ali_GanjiFY <ali.1385.ganji@gmail.com>
2026-06-22 21:08:40 +0330

    Initial commit

---
+++ Frontend/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.frontend to javafx.fxml;
+    exports org.example.frontend;
+}
