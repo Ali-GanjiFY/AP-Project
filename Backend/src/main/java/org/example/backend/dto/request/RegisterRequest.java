23149eea4eae18318c29491188137615e5a52627
Ali_GanjiFY <ali.1385.ganji@gmail.com>
2026-06-30 13:25:16 +0330

    feat: Add RegisterRequest to Request Package

---
+++ Backend/src/main/java/org/example/backend/dto/request/RegisterRequest.java
@@ -0,0 +1,73 @@
+package org.example.backend.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class RegisterRequest {
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "Phone number is required")
+    private String phone;
+
+    @Email(message = "Email format is invalid")
+    private String email;
+
+    // Constructor
+    public RegisterRequest() {
+    }
+
+    public RegisterRequest(String fullName, String username, String password, String phone, String email) {
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.phone = phone;
+        this.email = email;
+    }
+
+
+    // Getters and Setters
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
