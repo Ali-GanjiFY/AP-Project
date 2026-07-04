5b97eff1cc6fda678b3f9c52927806456c1834d8
Ali_GanjiFY <ali.1385.ganji@gmail.com>
2026-07-04 13:58:40 +0330

    feat: Add findByEmail and findByPhone to UserRepository

--- Backend/src/main/java/org/example/backend/repository/UserRepository.java
+++ Backend/src/main/java/org/example/backend/repository/UserRepository.java
@@ -11,6 +11,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhone(String phone);
     boolean existsByUsername(String username);
     boolean existsByPhone(String phone);
     boolean existsByEmail(String email);
